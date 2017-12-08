884c0cfe817cd5fa087b8a470454df0ef64f78cd
Bang Wu <ohwubang@gmail.com>
2017-12-09 00:50:09 +0800

    [bangwu] #N/A add the support the ios

--- ios/RNPhoneCall.h
+++ ios/RNPhoneCall.h
@@ -1,4 +1,7 @@
-
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <CallKit/CXCallObserver.h>
+#import <CallKit/CXCall.h>
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
@@ -6,6 +9,12 @@
 #endif
 
 @interface RNPhoneCall : NSObject <RCTBridgeModule>
+
+@property (nonatomic) CXCallObserver *callObserver;
+@property (nonatomic, strong) RCTPromiseResolveBlock resolve;
+@property (nonatomic, strong) RCTPromiseRejectBlock reject;
+@property (nonatomic) BOOL hasConnected;
+@property (nonatomic) NSNumber *callStartTime;
 
 @end
   
--- ios/RNPhoneCall.m
+++ ios/RNPhoneCall.m
@@ -9,5 +9,44 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_REMAP_METHOD(call,
+                 telephone: (NSString *)phoneNum
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+  NSURL *url= [NSURL URLWithString:[NSString stringWithFormat:@"telprompt://%@", phoneNum]];
+  [[UIApplication sharedApplication] openURL:url];
+  self.hasConnected = false;
+  self.reject = reject;
+  self.resolve = resolve;
+  self.callObserver = [[CXCallObserver alloc] init];
+  [self.callObserver setDelegate:self queue:nil];
+}
+
+-(NSNumber*) getCurrentTimeMillions {
+  NSTimeInterval timeStamp = [[NSDate date] timeIntervalSince1970];
+  return [NSNumber numberWithLong: timeStamp * 1000];
+}
+
+- (void)callObserver:(CXCallObserver *)callObserver callChanged:(CXCall *)call {
+  if (call.hasEnded && call.isOutgoing) {
+    if (self.hasConnected) {
+      NSNumber *endCallTime = [self getCurrentTimeMillions];
+      long duration = ([endCallTime integerValue] - [[self callStartTime] integerValue]) / 1000;
+      self.resolve(@{@"call_duration": [NSNumber numberWithLong: duration]});
+    } else {
+      self.reject(@"PhoneCall", @"Phone call is not connected", NULL);
+    }
+    [self.callObserver setDelegate:nil queue:nil];
+    self.callObserver = nil;
+    return;
+  }
+
+  if (call.hasConnected) {
+    self.hasConnected = true;
+    self.callStartTime = [self getCurrentTimeMillions];
+  }
+}
+
 @end
   
